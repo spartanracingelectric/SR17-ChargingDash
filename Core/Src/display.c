d6b2159f19a672e2d0328ad367fcd8998dee4f0c
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-02-20 17:53:43 -0800

    Create nav bar and Init code (#39)

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -1,3 +1,57 @@
 // Written by Ayman Alamayri in Dec 2024
+#include <stdbool.h>
 #include "display.h"
 #include "ssd1306.h"
+#include "ssd1306_fonts.h"
+#include <string.h>
+
+// Initialization function
+void SRE_Display_Init(bool test_mode) {
+	ssd1306_Init();
+	if (test_mode) {
+		SRE_Display_Test();
+	}
+}
+// Function to test display
+void SRE_Display_Test() {
+	ssd1306_Fill(White);
+	ssd1306_UpdateScreen();
+}
+// Example function to display navigation
+void SRE_Display_Nav() {
+
+}
+
+void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex) {
+
+	//the selectedButton values for nav bar will vary based on currenty displayed screen
+	int selectedButton = 0; //REMOVE THIS
+	int maxSelectedButtonIndex = firstButtonIndex + numOfButtons-1;
+	int buttonIndex = firstButtonIndex;
+
+	int x1 = 1;
+	int x2 = 1;
+
+	for (int i = 0; i < numOfButtons; i++) {
+		x2 = x1 + (strlen(buttons[i]) * 6) + 2;
+		if (selectedButton == buttonIndex ||
+			(buttonIndex == 0 && selectedButton > maxSelectedButtonIndex) ||
+			(buttonIndex == maxSelectedButtonIndex && selectedButton < 0))
+		{
+			ssd1306_FillRectangle(x1, 52, x2, 62, White);
+			ssd1306_SetCursor(x1 + 2, 54);
+			ssd1306_WriteString(buttons[i], Font_6x8, Black);
+		}
+		else {
+			ssd1306_DrawRectangle(x1, 52, x2, 62, White);
+			ssd1306_SetCursor(x1 + 2, 54);
+			ssd1306_WriteString(buttons[i], Font_6x8, White);
+		}
+
+		buttonIndex++;
+		x1 = x2 + 2;
+	}
+
+
+	ssd1306_UpdateScreen();
+}
--- Core/Src/display.h
+++ Core/Src/display.h
@@ -1,5 +1,10 @@
 // Written by Ayman Alamayri in Dec 2024
+#include <stdbool.h>
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
-
+void SRE_Display_Init(bool test_mode);
+void SRE_Display_Test();
+void SRE_Display_Nav();
+void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex);
 #endif
