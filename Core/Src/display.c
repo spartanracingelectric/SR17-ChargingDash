d9788f6d904710f49b6058b58d5ef2b28b48a454
Kevyi <kevin.hfan2023@gmail.com>
2025-02-23 22:35:55 -0800

    Added BatteryScreen 1 UI following prototype draft. (#44)

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -56,8 +56,80 @@ void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex
 		x1 = x2 + 2;
 	}
 
-
 	ssd1306_UpdateScreen();
+}
+
+void SRE_Display_Battery1(){
+	char battery1Title[] = "Battery 1";
+	char temperatureStats[] = "Tmp H/L:100.22/50.11C";
+	char voltageStats[] = "Vlt H/L:50.11/20.11V";
+	char averageStats[] = "Avg T/V:50.22C/20.11V";
+	char navButtonText[] = "Nav";
+	char battery2ButtonText[] = "Battery 2";
+
+	while(!selectPressed){
+
+		if (selectedButton > 1) {
+			selectedButton = 0;
+		}
+		if (selectedButton < 0) {
+			selectedButton = 1;
+		}
+
+			ssd1306_SetCursor(1, 2);
+			ssd1306_WriteString(battery1Title, Font_6x8, White);
+			//x1, y1, x2, y2
+				//Not sure if this makes a straight white line as no Cursor isn't selected.
+			//call setcursor function?
+			ssd1306_Line(0, 10, 127, 10, White);
+			//Everything appears to be incremented by Y = 10, so font is roughly 8 squares.
+			//Writes temp
+			ssd1306_SetCursor(1, 13);
+			ssd1306_WriteString(temperatureStats, Font_6x8, White);
+			//Writes voltage
+			ssd1306_SetCursor(1, 23);
+			ssd1306_WriteString(voltageStats, Font_6x8, White);
+			//Writes averageStats
+			ssd1306_SetCursor(1, 33);
+			ssd1306_WriteString(averageStats, Font_6x8, White);
+
+			//Writes button for Nav and selects.
+			if(selectedButton == 0 || selectedButton > 1){
+
+				ssd1306_SetCursor(3, 54);
+				//x1,y1,x2,y2
+				ssd1306_FillRectangle(1, 52, 24, 63, White);
+
+				//Has to be last so doesn't get filled.
+				ssd1306_WriteString(navButtonText, Font_6x8, Black);
+			}else{
+				ssd1306_SetCursor(3, 54);
+				ssd1306_WriteString(navButtonText, Font_6x8, White);
+				ssd1306_DrawRectangle(1, 52, 24, 63, White);
+			}
+
+			//Add 2 to x1 and x2 for spacing.
+			//Writes button for Battery 2 and selects
+			if(selectedButton == 1 || selectedButton < 0){
+				//Add 2px for padding for left.
+				ssd1306_SetCursor(28, 54);
+				ssd1306_FillRectangle(26, 52, 86, 63, White);
+
+				//Has to be last so doesn't get filled.
+				ssd1306_WriteString(battery2ButtonText, Font_6x8, Black);
+
+			}else{
+				//Add 2px for padding for left.
+				ssd1306_SetCursor(28, 54);
+				ssd1306_WriteString(battery2ButtonText, Font_6x8, White);
+				ssd1306_DrawRectangle(26, 52, 86, 63, White);
+			}
+
+			ssd1306_UpdateScreen();
+	}
+
+
+
 }
 
 void SRE_Display_Title_Bar(char title[]) {
@@ -95,3 +167,4 @@ void SRE_Display_Error_Symbol(int x, int y) {
 	ssd1306_Line(x, y+2, x, y+4, White);
 	ssd1306_Line(x, y+6, x, y+6, White);
 }
+
