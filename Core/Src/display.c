22e1f874003f363589456df637ef58d5b0c6add9
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-02-23 23:01:11 -0800

    Create start charging screen with scroll functionality (#42)

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -4,6 +4,7 @@
 #include "ssd1306.h"
 #include "ssd1306_fonts.h"
 #include <string.h>
+#include <stdio.h>
 extern int selectedButton;
 extern bool selectPressed;
 extern int backPressed;
@@ -57,6 +58,125 @@ void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex
 	}
 
 	ssd1306_UpdateScreen();
+}
+
+void SRE_Display_Start_Charging() {
+
+	//sets up sample profiles to use for testing
+	struct Profile {
+		char name[5];
+		int current;
+		int voltage;
+		bool isBalancing;
+	};
+
+	struct Profile p1 = {"P1", 10, 20, true};
+	struct Profile p2 = {"P2", 10, 400, true};
+	struct Profile p3 = {"P3", 10, 15, false};
+	struct Profile p4 = {"P4", 10, 20, true};
+	struct Profile p5 = {"P5", 10, 400, true};
+	struct Profile p6 = {"P6", 10, 15, false};
+	struct Profile p7 = {"P7", 10, 400, true};
+	struct Profile p8 = {"P8", 10, 15, false};
+
+	int numOfProfiles = 8;
+	struct Profile profiles[numOfProfiles];
+
+	profiles[0] = p1;
+	profiles[1] = p2;
+	profiles[2] = p3;
+	profiles[3] = p4;
+	profiles[4] = p5;
+	profiles[5] = p6;
+    profiles[6] = p7;
+    profiles[7] = p8;
+
+	while (!selectPressed) {
+
+		//resets screen
+		ssd1306_FillRectangle(0,0,127,63, Black);
+
+		SRE_Display_Title_Bar("Start Charging");
+
+		int currentScreen = selectedButton/3;
+		int navStartIndex = numOfProfiles;
+		int navLastIndex = numOfProfiles +2;
+
+		//if the nav bar is selected, ensures that the currentScreen is the last screen of profiles
+		if (selectedButton > numOfProfiles-1) {
+			currentScreen = (numOfProfiles-1)/3;
+		}
+		int startIndex = currentScreen*3;
+		//ensures that the correct number of profiles are showed on the last screen
+		if (selectedButton > numOfProfiles-1) {
+			startIndex = (numOfProfiles-1)/3*3;
+		}
+		//going up from first profile will go to Start button
+		if (selectedButton < 0) {
+			startIndex = 0;
+			selectedButton = navLastIndex;
+		}
+		//going down from start button will reset back to first profile being selected
+		if (selectedButton > navLastIndex) {
+			startIndex = 0;
+			selectedButton = 0;
+		}
+
+		//initial y-positions used for calculating profile display boxes
+		int y1 = 15;
+		int y2 = 13;
+		int y3 = 24;
+
+		//displays up to three profiles per screen
+		for (int i = startIndex; i < startIndex + 3 && i < numOfProfiles; i++) {
+			char profileString[50];
+			sprintf(profileString, "%s: %dA %dV BAL %s", profiles[i].name, profiles[i].current, profiles[i].voltage, profiles[i].isBalancing ? "ON" : "OFF");
+			ssd1306_SetCursor(3, y1);
+
+			if (selectedButton == i) {
+				ssd1306_FillRectangle(1,y2, 122, y3, White);
+				ssd1306_WriteString(profileString, Font_6x8, Black);
+			}
+			else {
+				ssd1306_DrawRectangle(1,y2, 122, y3, White);
+				ssd1306_WriteString(profileString, Font_6x8, White);
+			}
+
+			y1 = y1 + 12;
+			y2 = y2 + 12;
+			y3 = y3 + 12;
+		}
+
+		//numOfProiles + 2 ensures it will always round up
+		int numOfScreens = (numOfProfiles+2)/3;
+
+		SRE_Display_Short_Scroll_Bar(currentScreen, numOfScreens);
+
+		char *navBarButtons[] = {"Batt", "Nav", "Start"};
+		SRE_Display_Nav_Bar(navBarButtons,3, navStartIndex);
+
+
+
+		ssd1306_UpdateScreen();
+
+	}
+}
+
+void SRE_Display_Short_Scroll_Bar(int currentScreen, int numOfScreens) {
+
+	//currentScreen is zero-indexed
+
+	if (numOfScreens > 1) {
+
+		int scrollContainerHeight = 35;
+		int scrollBarLength = scrollContainerHeight/numOfScreens;
+
+		int scrollBarStart = 14+ (currentScreen*scrollBarLength);
+
+		ssd1306_DrawRectangle(124, 13, 126, 47, White);
+		ssd1306_Line(125, scrollBarStart, 125, scrollBarStart+scrollBarLength, White);
+	}
+
 }
 
 void SRE_Display_Charger_Stats() {
--- Core/Src/display.h
+++ Core/Src/display.h
@@ -7,6 +7,8 @@ void SRE_Display_Init(bool test_mode);
 void SRE_Display_Test();
 void SRE_Display_Nav();
 void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex);
+void SRE_Display_Start_Charging();
+void SRE_Display_Short_Scroll_Bar(int currentScreen, int numOfScreens);
 void SRE_Display_Charger_Stats();
 void SRE_Display_Battery1();
 void SRE_Display_Title_Bar(char title[]);
